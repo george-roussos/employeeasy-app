94d9d483f33a7851ddf8a4f30869781c81e323be
George Roussos <25833833+george-roussos@users.noreply.github.com>
2022-04-28 17:10:58 +0200

    Fix scrolling of table in Dashboard

--- src/components/Widgets/LatestRequests.js
+++ src/components/Widgets/LatestRequests.js
@@ -10,66 +10,69 @@ import {
 } from "@mui/material";
 
 import CircleIcon from "@mui/icons-material/Circle";
+import { makeStyles } from "@material-ui/core/styles";
 import { requests } from "../../helpers/widgetHelper";
 
-export const LatestRequests = () => (
-  <Card>
-    <CardHeader
-      titleTypographyProps={{ variant: "h4" }}
-      title="Latest Requests"
-    />
-    <Box sx={{ width: "700", scroll: "auto" }}>
-      <Table>
-        <TableHead>
-          <TableRow
-            style={{
-              backgroundColor: "#f5f5f5",
-            }}
-          >
-            <TableCell sx={{ fontSize: "1.5rem" }}>Project</TableCell>
-            <TableCell sx={{ fontSize: "1.5rem" }}>From</TableCell>
-            <TableCell sx={{ fontSize: "1.5rem" }}>Date</TableCell>
-            <TableCell sx={{ fontSize: "1.5rem" }}>Assignee</TableCell>
-            <TableCell sx={{ fontSize: "1.5rem" }}>Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {requests.map((request) => (
-            <TableRow hover key={request.id}>
-              <TableCell sx={{ fontSize: "1.5rem" }}>{request.ref}</TableCell>
-              <TableCell sx={{ fontSize: "1.5rem" }}>
-                {request.from.name}
-              </TableCell>
-              <TableCell sx={{ fontSize: "1.5rem" }}>
-                {request.createdAt}
-              </TableCell>
-              <TableCell sx={{ fontSize: "1.5rem" }}>
-                {request.assignee}
-              </TableCell>
-              <TableCell sx={{ fontSize: "1.5rem" }}>
-                <CircleIcon
-                  style={{
-                    fontSize: "1rem",
-                    color:
-                      request.status === "active"
-                        ? "lightblue"
-                        : request.status === "blocked"
-                        ? "red"
-                        : request.status === "forwarded"
-                        ? "purple"
-                        : "lightgreen",
-                  }}
-                />
-                {"  "}
-                {request.status.charAt(0).toUpperCase()}
-                {request.status.slice(1)}
-              </TableCell>
+const LatestRequests = () => {
+  return (
+    <Card>
+      <CardHeader
+        titleTypographyProps={{ variant: "h4" }}
+        title="Latest Requests"
+      />
+      <Box sx={{ width: "800", overflowX: "auto" }}>
+        <Table>
+          <TableHead>
+            <TableRow
+              style={{
+                backgroundColor: "#f5f5f5",
+              }}
+            >
+              <TableCell sx={{ fontSize: "1.5rem" }}>Project</TableCell>
+              <TableCell sx={{ fontSize: "1.5rem" }}>From</TableCell>
+              <TableCell sx={{ fontSize: "1.5rem" }}>Date</TableCell>
+              <TableCell sx={{ fontSize: "1.5rem" }}>Assignee</TableCell>
+              <TableCell sx={{ fontSize: "1.5rem" }}>Status</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Box>
-  </Card>
-);
+          </TableHead>
+          <TableBody>
+            {requests.map((request) => (
+              <TableRow hover key={request.id}>
+                <TableCell sx={{ fontSize: "1.5rem" }}>{request.ref}</TableCell>
+                <TableCell sx={{ fontSize: "1.5rem" }}>
+                  {request.from.name}
+                </TableCell>
+                <TableCell sx={{ fontSize: "1.5rem" }}>
+                  {request.createdAt}
+                </TableCell>
+                <TableCell sx={{ fontSize: "1.5rem" }}>
+                  {request.assignee}
+                </TableCell>
+                <TableCell sx={{ fontSize: "1.5rem" }}>
+                  <CircleIcon
+                    style={{
+                      fontSize: "1rem",
+                      color:
+                        request.status === "active"
+                          ? "lightblue"
+                          : request.status === "blocked"
+                          ? "red"
+                          : request.status === "forwarded"
+                          ? "purple"
+                          : "lightgreen",
+                    }}
+                  />
+                  {"  "}
+                  {request.status.charAt(0).toUpperCase()}
+                  {request.status.slice(1)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </Card>
+  );
+};
 
 export default LatestRequests;
