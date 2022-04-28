9ef19b5bcc2144ceaa20182b6b91e07e9b3611df
George Roussos <25833833+george-roussos@users.noreply.github.com>
2022-04-28 14:01:34 +0200

    Remove unused services and stylings

--- src/components/Forms/controls/Controls.js
+++ src/components/Forms/controls/Controls.js
@@ -1,15 +1,11 @@
-import Input from "./Input";
-import RadioGroup from "./RadioGroup";
-import Select from "./Select";
-import Checkbox from "./Checkbox";
-import DatePicker from "./DatePicker";
 import Button from "./Button";
+import DatePicker from "./DatePicker";
+import Input from "./Input";
+import Select from "./Select";
 
 const Controls = {
     Input,
-    RadioGroup,
     Select,
-    Checkbox,
     DatePicker,
     Button
 
